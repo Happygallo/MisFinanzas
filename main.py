09b9f442212b5a895b9f71f6139f03f393f94994
Javier Gallo <javier@javiergalloroca.com>
2020-12-13 16:04:36 -0500

    Corregir main

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
-from fastapi import FastAPI, Request, HTTPException 
-from fastapi.templating import Jinja2Templates
-from db.user_db import get_user, post_user, UserDB
+from db.user_db import UserInDB
+from db.user_db import update_user, get_user
+from db.transaction_db import TransactionInDB
+from db.transaction_db import save_transaction
 from models.user_models import UserIn, UserOut
-app = FastAPI()
+from models.transaction_models import TransactionIn, TransactionOut
+import datetime
+from fastapi import FastAPI, HTTPException
 
-templates = Jinja2Templates(directory="layout")
-
-@app.get("/")
-def inicio(request: Request):
-    """
-    inicio de la aplicacion, petición de usuario
-    """
-    return templates.TemplateResponse("inicio.html", {
-        "request": request,
-        "variable": "MinTIC 2022"
-    })
-
+api = FastAPI()
 
 @app.get("/usuario/{username}")
 async def display_user(username: str, request: Request):
