c0ca5f493423ab09d5f8b9726806db26fa51b279
Santiago Vivas <sago.vivas@gmail.com>
2020-12-17 15:03:04 -0500

    CORS agregado

--- main.py
+++ main.py
@@ -8,6 +8,14 @@ from fastapi.middleware.cors import CORSMiddleware
 
 api = FastAPI()
 
+origins = [
+    "http://localhost", "http://localhost:8080",
+]
+api.add_middleware(
+    CORSMiddleware, allow_origins=origins,
+    allow_credentials=True, allow_methods=["*"], allow_headers=["*"],
+)
+
 # página de inicio
 @api.get("/")
 def inicio():
