ff59422095124ec80bf5c8fc153f9a94fdc12773
Javier Gallo <javier@javiergalloroca.com>
2020-12-19 03:25:08 -0500

    Actualizar CORS

--- main.py
+++ main.py
@@ -8,6 +8,22 @@ from fastapi.middleware.cors import CORSMiddleware
 
 api = FastAPI()
 
+origins = [
+     "http://localhost", 
+     "http://localhost:8080",
+     "http://app-misfinanzas.herokuapp.com",
+     "https://app-misfinanzas.herokuapp.com"
+
+]
+
+api.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"],
+)
+
 # página de inicio
 @api.get("/")
 def inicio():
